5247f964e8bf828e90fa56515ded67df783ca385
Ernest Ng <eyn5@cornell.edu>
2024-12-01 11:49:39 -0500

    day 1 done!

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -22,6 +22,9 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
+-- Note: Attoparsec's `parseOnly` function (in `RunDay.hs`) doesn't make
+-- its argument to consume all its input, so we need to post-compose
+-- this parser with `endOfInput` at the end
 inputParser :: Parser [(Int, Int)]
 inputParser = sepBy1 (UP.around decimal skipSpace) endOfLine <* endOfInput
 
@@ -30,11 +33,26 @@ inputParser = sepBy1 (UP.around decimal skipSpace) endOfLine <* endOfInput
 mapTuple :: (a -> b) -> (a, a) -> (b, b)
 mapTuple f (a1, a2) = (f a1, f a2)
 
+-- | Takes the input list of pairs, and returns a pair of sorted lists
+sortedPairs :: [(Int, Int)] -> ([Int], [Int])
+sortedPairs pairs = mapTuple sort (unzip pairs)
+
+-- | Computes the sum of the differences of the `i`-th largest element
+-- of each list
 partA :: [(Int, Int)] -> Int
 partA pairs = sum [abs (x - y) | (x, y) <- zip xs ys]
   where
-    (xs, ys) = mapTuple sort (unzip pairs)
+    (xs, ys) = sortedPairs pairs
 
 ------------ PART B ------------
-partB :: [(Int, Int)] -> Void
-partB = error "Not implemented yet!"
+
+-- | Computes the sum of all the elements of `xs`, where each element
+-- is multiplied by its no. of occurences in `ys`
+partB :: [(Int, Int)] -> Int
+partB pairs = Map.foldrWithKey (\x n acc -> x * n + acc) 0 frequencies
+  where
+    (xs, ys) = sortedPairs pairs
+
+    -- Frequency of each element of `xs` that also appeared in `ys`
+    frequencies :: Map Int Int
+    frequencies = U.freq (filter (`elem` xs) ys)
