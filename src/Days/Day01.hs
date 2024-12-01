c6a2f8fa397bdc21a5531786afe040040e734f42
Ernest Ng <eyn5@cornell.edu>
2024-12-01 11:38:14 -0500

    Part 1 of day 1

--- src/Days/Day01.hs
+++ src/Days/Day01.hs
@@ -11,6 +11,8 @@ import Data.Vector (Vector)
 import qualified Data.Vector as Vec
 import qualified Util.Util as U
 
+import qualified Util.Parsers as UP
+
 import qualified Program.RunDay as R (runDay, Day)
 import Data.Attoparsec.Text
 import Data.Void
@@ -20,20 +22,19 @@ runDay :: R.Day
 runDay = R.runDay inputParser partA partB
 
 ------------ PARSER ------------
-inputParser :: Parser Input
-inputParser = error "Not implemented yet!"
-
------------- TYPES ------------
-type Input = Void
-
-type OutputA = Void
-
-type OutputB = Void
+inputParser :: Parser [(Int, Int)]
+inputParser = sepBy1 (UP.around decimal skipSpace) endOfLine <* endOfInput
 
 ------------ PART A ------------
-partA :: Input -> OutputA
-partA = error "Not implemented yet!"
+
+mapTuple :: (a -> b) -> (a, a) -> (b, b)
+mapTuple f (a1, a2) = (f a1, f a2)
+
+partA :: [(Int, Int)] -> Int
+partA pairs = sum [abs (x - y) | (x, y) <- zip xs ys]
+  where
+    (xs, ys) = mapTuple sort (unzip pairs)
 
 ------------ PART B ------------
-partB :: Input -> OutputB
+partB :: [(Int, Int)] -> Void
 partB = error "Not implemented yet!"
