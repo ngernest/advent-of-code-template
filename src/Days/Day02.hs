16436cc80559251455be8c1254943439dbff6f3d
Ernest Ng <eyn5@cornell.edu>
2024-12-02 23:48:11 -0500

    day 2 done!

--- src/Days/Day02.hs
+++ src/Days/Day02.hs
@@ -32,25 +32,23 @@ singleSpaceP = skip isHorizontalSpace
 inputParser :: Parser [[Int]]
 inputParser = sepBy1 (sepBy1 decimal singleSpaceP) endOfLine <* endOfInput
 
------------- TYPES ------------
-type Input = Void
-
-type OutputA = Void
-
-type OutputB = Void
-
 ------------ PART A ------------
 
+-- | Checks whether an int is within the specified range
 diffIsOk :: Int -> Bool
 diffIsOk d = absD >= 1 && absD <= 3
   where
     absD = abs d
 
+-- | Checks whether a row is safe by examining `diffs`
+-- (the list of differences between successive elements)
 isSafe :: [Int] -> Bool
 isSafe xs = foldr (\d acc -> diffIsOk d && acc) True diffs && correctSign diffs
   where
+    diffs :: [Int]
     diffs = mkDiffs xs
 
+    -- Checks that all the diffs are either all negative or all positive
     correctSign :: [Int] -> Bool
     correctSign diffs = all (< 0) diffs || all (> 0) diffs
 
@@ -58,9 +56,37 @@ isSafe xs = foldr (\d acc -> diffIsOk d && acc) True diffs && correctSign diffs
 mkDiffs :: [Int] -> [Int]
 mkDiffs xs = zipWith (-) (tail xs) xs
 
+-- | Computes the no. of safe rows
 partA :: [[Int]] -> Int
 partA rows = length (filter isSafe rows)
 
 ------------ PART B ------------
-partB :: [[Int]] -> OutputB
-partB = error "Not implemented yet!"
+partB :: [[Int]] -> Int
+partB rows = prevAns + length (filter id safetyOfCandidates)
+  where
+    prevAns :: Int
+    prevAns = partA rows
+
+    -- First, find all the rows that are unsafe
+    unsafeRows :: [[Int]]
+    unsafeRows = filter (not . isSafe) rows
+
+    -- For each row, find all the ways to remove an element from that row
+    -- (call each of these resultant lists a "candidate")
+    removalCandidates :: [[[Int]]]
+    removalCandidates = map allRemovals unsafeRows
+
+    -- For each row, check if there exists a candidate that is now safe
+    safetyOfCandidates :: [Bool]
+    safetyOfCandidates = map (any isSafe) removalCandidates
+
+-- | All ways to remove an element from a list
+allRemovals :: [Int] -> [[Int]]
+allRemovals xs = do
+  let n = length xs
+  i <- [0 .. n - 1]
+
+  -- Compute all possible ways to partition the list at index `i`
+  let (prefix, suffix) = splitAt i xs
+
+  return $ prefix ++ drop 1 suffix
